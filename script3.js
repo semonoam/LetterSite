c1c9537d1cd65402c1129c40c242845aebd8c87c
semonoam <85457380+semonoam@users.noreply.github.com>
2023-05-26 18:17:07 +0200

    add stage 3 sliding

--- script3.js
+++ script3.js
@@ -1,7 +1,34 @@
-document.addEventListener("scroll", function (scrolled) {
-  var currentPixels = window.scrollY;
+document.body.style.width = '8000px';
+document.body.style.height = '100%';
+//document.body.style.overflowX = 'scroll';
+//document.body.style.overflowY = 'hidden';
 
-    if (currentPixels > 1700) {
-    window.location.href = "stage4.html";
-  }
+document.addEventListener("scroll", function (scrolled) {
+
 });
+
+
+window.addEventListener('DOMContentLoaded', function() {
+  var scrolling = false;
+
+  document.addEventListener('wheel', function(event) {
+    //event.preventDefault();
+    console.log('Im WHEEL');
+      console.log('Im TRUE');
+      scrolling = true;
+      var scrollAmount = event.deltaY;
+      console.log('scrollAmount', scrollAmount + 'px');
+      window.scrollBy({
+        left: scrollAmount,
+      });
+      var currentPixels = window.scrollX;
+      console.log('currentPixels', currentPixels + 'px');
+      console.log('document.body.style.width * 0.7', (document.body.clientWidth * 0.7) + 'px');
+
+        if (currentPixels > (document.body.clientWidth * 0.7)) {
+          console.log('Im 4');
+          window.location.href = "stage4.html";
+      }
+  });
+});
+
